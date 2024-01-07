cdf92fb1cf7d979703fa06abfd42c51af2ffc8d6
rockbiter <74297920+rock-biter@users.noreply.github.com>
2024-01-07 14:03:08 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,102 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import * as dat from 'lil-gui'
+
+/**
+ * Debug
+ */
+// const gui = new dat.GUI()
+
+/**
+ * Scene
+ */
+const scene = new THREE.Scene()
+
+/**
+ * BOX
+ */
+const material = new THREE.MeshNormalMaterial()
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+/**
+ * render sizes
+ */
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+}
+/**
+ * Camera
+ */
+const fov = 60
+const camera = new THREE.PerspectiveCamera(fov, sizes.width / sizes.height, 0.1)
+camera.position.set(4, 4, 4)
+camera.lookAt(new THREE.Vector3(0, 2.5, 0))
+
+/**
+ * Show the axes of coordinates system
+ */
+const axesHelper = new THREE.AxesHelper(3)
+scene.add(axesHelper)
+
+/**
+ * renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+	antialias: window.devicePixelRatio < 2,
+	logarithmicDepthBuffer: true,
+})
+document.body.appendChild(renderer.domElement)
+handleResize()
+
+/**
+ * OrbitControls
+ */
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+/**
+ * Three js Clock
+ */
+// const clock = new THREE.Clock()
+
+/**
+ * frame loop
+ */
+function tic() {
+	/**
+	 * tempo trascorso dal frame precedente
+	 */
+	// const deltaTime = clock.getDelta()
+	/**
+	 * tempo totale trascorso dall'inizio
+	 */
+	// const time = clock.getElapsedTime()
+
+	controls.update()
+
+	renderer.render(scene, camera)
+
+	requestAnimationFrame(tic)
+}
+
+requestAnimationFrame(tic)
+
+window.addEventListener('resize', handleResize)
+
+function handleResize() {
+	sizes.width = window.innerWidth
+	sizes.height = window.innerHeight
+
+	camera.aspect = sizes.width / sizes.height
+	camera.updateProjectionMatrix()
+
+	renderer.setSize(sizes.width, sizes.height)
+
+	const pixelRatio = Math.min(window.devicePixelRatio, 2)
+	renderer.setPixelRatio(pixelRatio)
+}
